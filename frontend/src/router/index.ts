fbefab4f4e6bc693eab56b1b4d6beb2031c0fc40
GrafRobert <grrobert2024@gmail.com>
2026-03-23 20:26:14 +0200

    OrderingView component and configure router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import OrderingView from '@/view/OrderingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: OrderingView
+    }
+  ],
 })
 
 export default router
